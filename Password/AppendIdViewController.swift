40b60aa785ef8bfec5d31acea26572f675978200
LeeeeSuHyeon <tngus0673@naver.com>
2022-08-11 18:11:00 +0900

    Feat Append Id ViewController

---
+++ Password/AppendIdViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AppendIdViewController.swift
+//  Password
+//
+//  Created by 이수현 on 2022/08/11.
+//
+
+import UIKit
+
+class AppendIdViewController: UIViewController {
+
+
+    @IBAction func back(_ segue: UIStoryboardSegue) {
+       // self.performSegue(withIdentifier: "Append", sender: self)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
